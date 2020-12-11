95b0641d9e573fde42967eaaf655a095adb38122
Cristina VOINEAG (67179) <cristina.voineag@stud.acs.upb.ro>
2020-12-11 14:49:23 +0200

    check first if there is a process running

--- keep_ns_up.py
+++ keep_ns_up.py
@@ -2,7 +2,23 @@
 
 import subprocess
 import time
+import psutil
+
+
+def checkIfProcessRunning(processName):
+
+    #Iterate over the all the running process
+    for proc in psutil.process_iter():
+        try:
+            # Check if process name contains the given name string.
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess) as e:
+            pass
+    return False
+
 
 while(True):
-	subprocess.call(["npm", "start"])
-	time.sleep(900) # 15 minutes
+	if (checkIfProcessRunning("npm") == False):
+		subprocess.call(["npm", "start"])
+	time.sleep(600) # 10 minutes
